908a3936635dbcd2626697c67b5261831b995a8d
Noori78692 <noorimisbahi96@gmail.com>
2026-07-24 02:56:06 -0700

    Add Android hardware back button support

--- artifacts/grammar-course/src/main.tsx
+++ artifacts/grammar-course/src/main.tsx
@@ -1,9 +1,20 @@
 import { createRoot } from "react-dom/client";
+import { App as CapacitorApp } from "@capacitor/app";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
 
+// Android Hardware Back Button
+CapacitorApp.addListener("backButton", () => {
+  if (window.location.pathname !== "/") {
+    window.history.back();
+  } else {
+    CapacitorApp.exitApp();
+  }
+});
+
+// Service Worker
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
